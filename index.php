4e98b0219f467554898b94eada129362abbfb880
fedtoscano <federicotoscano611@gmail.com>
2024-06-26 12:19:10 +0200

    creato array con le squadre

---
+++ index.php
@@ -0,0 +1,51 @@
+<!-- Snack 1
+Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60 -->
+
+<?php 
+    $bastekMatches = [
+        [
+            'casa' => "Inter",
+            'ospite' => 'Roma',
+            'puntiCasa' => '30',
+            'puntiOspite' => '12',
+        ],
+        [
+            'casa' => "Milano",
+            'ospite' => 'Sassuolo',
+            'puntiCasa' => '12',
+            'puntiOspite' => '5',
+        ],
+        [
+            'casa' => "Juventus",
+            'ospite' => 'Catania',
+            'puntiCasa' => '155',
+            'puntiOspite' => '54',
+        ],
+        [
+            'casa' => "Empoli",
+            'ospite' => 'Genoa',
+            'puntiCasa' => '12',
+            'puntiOspite' => '7',
+        ],
+        [
+            'casa' => "Sampdoria",
+            'ospite' => 'Atletico',
+            'puntiCasa' => '99',
+            'puntiOspite' => '98',
+        ],
+    ];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
