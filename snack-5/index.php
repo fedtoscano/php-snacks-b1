5b5c28f87b30d5b50749beee019279167e6c496b
fedtoscano <federicotoscano611@gmail.com>
2024-06-26 18:15:40 +0200

    snack 5

---
+++ snack-5/index.php
@@ -0,0 +1,5 @@
+<?php
+$paragraph = 'Io sono una persona malata… sono una persona cattiva. Io sono uno che non ha niente di attraente. Credo di avere una malattia al fegato. Anche se da altra parte non ci capisco una acca della mia malattia, e non so che cosa precisamente ci sia di malato in me. Non mi curo e non mi sono mai curato, anche se la medicina e i dottori io li rispetto. Per di più sono anche superstizioso al massimo grado; o per lo meno quanto basta per rispettare la medicina.';
+$phraseList = explode('.', $paragraph);
+
+var_dump($phraseList);
