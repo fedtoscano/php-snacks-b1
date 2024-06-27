d8c2e9eebbcfd157da9a4f019934f2bc441d2f6b
fedtoscano <federicotoscano611@gmail.com>
2024-06-27 12:27:15 +0200

    file studentsList correttamente importato

---
+++ snack-7/index.php
@@ -0,0 +1,11 @@
+Snack 7
+Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+Questo array dovra' essere inserito in un file separato, da importare.
+Stampare Nome, Cognome e la media dei voti di ogni alunno.
+
+<?php
+    include __DIR__."/studentsList.php";
+
+    // var_dump($students)
+?>
+
---
+++ snack-7/studentsList.php
@@ -0,0 +1,34 @@
+<?php
+    $students = [
+        [
+            'name'=> 'Federico',
+            'lastName' => 'Toscano',
+            'grades' => [
+                'math' => 8,
+                'geography' => 6,
+                'history' => 4,
+                'literature' => 10
+            ]
+        ],
+        [
+            'name'=> 'Giggina',
+            'lastName' => 'Gigioneggi',
+            'grades' => [
+                'math' => 4,
+                'geography' => 10,
+                'history' => 6,
+                'literature' => 8
+            ]
+        ],
+        [
+            'name'=> 'Giangizio',
+            'lastName' => 'Giengiskan',
+            'grades' => [
+                'math' => 2,
+                'geography' => 2,
+                'history' => 2,
+                'literature' => 2,
+            ]
+        ],
+    ];
+?>
