47959eed80d5a3ba4bbc3ff6bc31d4a03deff5de
fedtoscano <federicotoscano611@gmail.com>
2024-06-27 12:36:05 +0200

    snack completato

--- snack-7/index.php
+++ snack-7/index.php
@@ -5,7 +5,33 @@ Stampare Nome, Cognome e la media dei voti di ogni alunno.
 
 <?php
     include __DIR__."/studentsList.php";
+    
 
     // var_dump($students)
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Students:</h1>
+        <ul>
+            <?php foreach ($students as $key => $student) { ?>
+                <li>
+                    <?php echo $student['name'].' '.$student['lastName'] ?>
+                    <h3>Media dei voti:</h3>
+                    <?php
+                        $gradeSum = array_sum($student['grades']);
+                        $average = $gradeSum / count($student['grades']);
+
+                        echo $average;
+                    ?>
+                </li>
+            <?php }?>
+        </ul>
+</body>
+</html>
